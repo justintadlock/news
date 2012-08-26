506ff352a2f0497c5fa1946851804725472d3387
Justin Tadlock <justin@justintadlock.com>
2012-08-26 11:16:21 -0500

    Add support for previous_image_link() and next_image_link() to satisfy WordPress.org requirements.

--- loop-nav.php
+++ loop-nav.php
@@ -17,6 +17,13 @@
 			<?php next_post_link( '%link', '<span class="next">' . __( 'Next &rarr;', 'news' ) . '</span>' ); ?>
 		</div><!-- .navigation-links -->
 
+	<?php elseif ( is_attachment() ) : ?>
+
+		<div class="loop-nav navigation-links">
+			<?php previous_image_link( false, '<span class="previous">' . __( '&larr; Previous', 'news' ) . '</span>' ); ?>
+			<?php next_image_link( false, '<span class="next">' . __( 'Next &rarr;', 'news' ) . '</span>' ); ?>
+		</div><!-- .navigation-links -->
+
 	<?php elseif ( !is_singular() && current_theme_supports( 'loop-pagination' ) ) : loop_pagination(); ?>
 
 	<?php elseif ( !is_singular() && $nav = get_posts_nav_link( array( 'sep' => '', 'prelabel' => '<span class="previous">' . __( '&larr; Previous', 'news' ) . '</span>', 'nxtlabel' => '<span class="next">' . __( 'Next &rarr;', 'news' ) . '</span>' ) ) ) : ?>
